323224da8bd61d6f2698c2b1661cf1590f34a630
Stanislav Kalinin <kalinin@sent.com>
2021-12-29 16:57:33 +0300

    feat: Modify evaluation script

--- scripts/evaluate.py
+++ scripts/evaluate.py
@@ -1,13 +1,11 @@
-import cv2
 import argparse
 import os
 import json
 from tqdm import tqdm
 import numpy as np
 
-from ocrpipeline.predictor import PipelinePredictor
 from ocrpipeline.utils import AverageMeter
-from ocrpipeline.metrics import get_accuracy, cer, wer, iou_bbox
+from ocrpipeline.metrics import get_accuracy, cer, wer, iou_bbox, iou_polygon
 
 
 def numbers2coords(list_of_numbers):
@@ -15,21 +13,6 @@ def numbers2coords(list_of_numbers):
     bbox = [[list_of_numbers[i], list_of_numbers[i+1]]
             for i in range(0, len(list_of_numbers), 2)]
     return np.array(bbox)
-
-
-def upscale_bbox(bbox, upscale_x=1, upscale_y=1):
-    """Increase size of the bbox."""
-    height = bbox[3] - bbox[1]
-    width = bbox[2] - bbox[0]
-
-    y_change = (height * upscale_y) - height
-    x_change = (width * upscale_x) - width
-
-    x_min = max(0, bbox[0] - int(x_change/2))
-    y_min = max(0, bbox[1] - int(y_change/2))
-    x_max = bbox[2] + int(x_change/2)
-    y_max = bbox[3] + int(y_change/2)
-    return x_min, y_min, x_max, y_max
 
 
 def polygon2bbox(polygon):
@@ -51,7 +34,7 @@ def polygon2bbox(polygon):
 
 def get_data_from_image(data, image_id, category_ids):
     texts = []
-    bboxes = []
+    polygons = []
     for idx, data_ann in enumerate(data['annotations']):
         if (
             data_ann['image_id'] == image_id
@@ -61,35 +44,39 @@ def get_data_from_image(data, image_id, category_ids):
             and data_ann['segmentation']
         ):
             polygon = numbers2coords(data_ann['segmentation'][0])
-            bbox = polygon2bbox(polygon)
-            bboxes.append(bbox)
+            polygons.append(polygon)
             texts.append(data_ann['attributes']['translation'])
-    return texts, bboxes
+    return texts, polygons
 
 
-def get_pred_text_for_gt_bbox(gt_bbox, pred_data):
+def get_pred_text_for_gt_polygon(gt_polygon, pred_data, evaluate_by_bbox):
     max_iou = 0
     pred_text_for_gt_bbox = ''
-    for prediction in pred_data['predictions']:
-        bbox = prediction['bbox']
-        pred_text = prediction['text']
-        iou = iou_bbox(gt_bbox, bbox)
-        if iou > max_iou:
-            max_iou = iou
-            pred_text_for_gt_bbox = pred_text
+    matching_idx = None
+    gt_bbox = polygon2bbox(gt_polygon)
+    for idx, prediction in enumerate(pred_data['predictions']):
+        if prediction.get('matched') is None:
+            polygon = prediction['polygon']
+            pred_text = prediction['text']
+            if evaluate_by_bbox:
+                bbox = polygon2bbox(polygon)
+                iou = iou_bbox(gt_bbox, bbox)
+            else:
+                iou = iou_polygon(gt_polygon, polygon)
+
+            if iou > max_iou:
+                max_iou = iou
+                pred_text_for_gt_bbox = pred_text
+                matching_idx = idx
+
+    # to prevent matching one predicted bbox to several ground true bboxes
+    if matching_idx is not None:
+        pred_data['predictions'][matching_idx]['matched'] = True
     return pred_text_for_gt_bbox
 
 
 def main(args):
-    predictor = PipelinePredictor(
-        segm_model_path=args.segm_model_path,
-        segm_config_path=args.segm_config_path,
-        ocr_model_path=args.ocr_model_path,
-        ocr_config_path=args.ocr_config_path,
-        device=args.device
-    )
-
-    with open(args.data_json_path, 'r') as f:
+    with open(args.annotation_json_path, 'r') as f:
         data = json.load(f)
 
     acc_avg = AverageMeter()
@@ -98,23 +85,36 @@ def main(args):
     for data_img in tqdm(data['images']):
         img_name = data_img['file_name']
         image_id = data_img['id']
-        image = cv2.imread(os.path.join(args.image_root, img_name))
 
-        texts_from_image, bboxes_from_image = \
+        texts_from_image, polygons_from_image = \
             get_data_from_image(data, image_id, args.category_ids)
 
-        pred_data = predictor(image)
+        pred_json_name = os.path.splitext(img_name)[0] + '.json'
+        pred_json_path = os.path.join(args.pred_jsons_dir, pred_json_name)
+        with open(pred_json_path, 'r') as f:
+            pred_data = json.load(f)
 
+        # find predicted text for each ground true polygon
         pred_texts = []
-        for gt_bbox in bboxes_from_image:
+        for gt_polygon in polygons_from_image:
             pred_texts.append(
-                get_pred_text_for_gt_bbox(gt_bbox, pred_data)
+                get_pred_text_for_gt_polygon(
+                    gt_polygon, pred_data, args.evaluate_by_bbox)
             )
 
-        num_imgs = len(pred_texts)
-        acc_avg.update(get_accuracy(texts_from_image, pred_texts), num_imgs)
-        wer_avg.update(wer(texts_from_image, pred_texts), num_imgs)
-        cer_avg.update(cer(texts_from_image, pred_texts), num_imgs)
+        # to penalty false positive prediction, that were not matched with gt
+        for prediction in pred_data['predictions']:
+            if (
+                prediction.get('matched') is None
+                and prediction['text']  # ignore empty prediction
+            ):
+                pred_texts.append(prediction['text'])
+                texts_from_image.append('')
+
+        num_samples = len(pred_texts)
+        acc_avg.update(get_accuracy(texts_from_image, pred_texts), num_samples)
+        wer_avg.update(wer(texts_from_image, pred_texts), num_samples)
+        cer_avg.update(cer(texts_from_image, pred_texts), num_samples)
 
     print(f'acc: {acc_avg.avg:.4f}')
     print(f'wer: {wer_avg.avg:.4f}')
@@ -123,24 +123,16 @@ def main(args):
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('--segm_model_path', type=str, required=True,
-                        help='Path to segmentation model weights.')
-    parser.add_argument('--segm_config_path', type=str, required=True,
-                        help='Path to segmentation config json.')
-    parser.add_argument('--ocr_model_path', type=str, required=True,
-                        help='Path to OCR model weights.')
-    parser.add_argument('--ocr_config_path', type=str, required=True,
-                        help='Path to OCR config json.')
-    parser.add_argument('--device', type=str, default='cuda',
-                        help='Device to use: cuda or cpu')
-    parser.add_argument('--data_json_path', type=str, required=True,
+    parser.add_argument('--annotation_json_path', type=str, required=True,
                         help='Path to json with segmentation dataset'
                         'annotation in COCO format')
-    parser.add_argument('--image_root', type=str, required=True,
-                        help='Path to folder with evaluation images')
     parser.add_argument('--category_ids', nargs='+', type=int, required=True,
                         help='Category indexes (separated by spaces) from '
-                        'data_json_path for evaluation.')
+                        'annotation_json_path for evaluation.')
+    parser.add_argument('--pred_jsons_dir', type=str, required=True,
+                        help='Path to folder with predicted json for each image.')
+    parser.add_argument('--evaluate_by_bbox', action='store_true',
+                        help='To evaluate prediction by bbox.')
     args = parser.parse_args()
 
     main(args)
