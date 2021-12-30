7d911a7801722e4eccab2c04fff01c6e01e26276
Stanislav Kalinin <kalinin@sent.com>
2021-12-30 12:48:23 +0300

    docs: Add docstring to evaluation metric

--- scripts/evaluate.py
+++ scripts/evaluate.py
@@ -75,8 +75,52 @@ def get_pred_text_for_gt_polygon(gt_polygon, pred_data, evaluate_by_bbox):
     return pred_text_for_gt_bbox
 
 
-def main(args):
-    with open(args.annotation_json_path, 'r') as f:
+def evaluate_pipeline(
+    annotation_json_path, category_ids, pred_jsons_dir, evaluate_by_bbox
+):
+    """
+    Calculate the evaluation metric for the pipeline by matching the
+    predicted segmentation and the ground truth bboxes (or polygons) and thus
+    comparing the predicted OCR texts and the gt texts.
+
+    Args:
+        category_ids (list of inst): a list with category indexes.
+        evaluate_by_bbox (bool): To evaluate prediction by bbox. If False -
+            evaluation by polygons.
+        pred_jsons_dir (str): Path to folder with predicted json for each image.
+            Each json should have name of the image from annotation_json_path
+            and have format like this:
+            {
+                "predictions": [
+                    {
+                        "polygon": list,  # the coordinates of the polygon
+                        "text": str  # predicted text
+                    },
+                    ...
+                ]
+            }
+        annotation_json_path (str): Path to the segmentation annotation json
+            in COCO format. The json should have the following format:
+            {
+                "images": [
+                    {
+                        "file_name": str,  # name of the image file
+                        "id": int  # image id
+                    },
+                    ...
+                ],
+                "annotations": [
+                    {
+                        "image_id": int,  # the index of the image on which the polygon is located
+                        "category_id": int,  # the polygon’s category index
+                        "attributes": {"translation": str},  # text in the polygon
+                        "segmentation": list  # the coordinates of the polygon
+                    },
+                    ...
+                ]
+            }
+    """
+    with open(annotation_json_path, 'r') as f:
         data = json.load(f)
 
     acc_avg = AverageMeter()
@@ -87,10 +131,10 @@ def main(args):
         image_id = data_img['id']
 
         texts_from_image, polygons_from_image = \
-            get_data_from_image(data, image_id, args.category_ids)
+            get_data_from_image(data, image_id, category_ids)
 
         pred_json_name = os.path.splitext(img_name)[0] + '.json'
-        pred_json_path = os.path.join(args.pred_jsons_dir, pred_json_name)
+        pred_json_path = os.path.join(pred_jsons_dir, pred_json_name)
         with open(pred_json_path, 'r') as f:
             pred_data = json.load(f)
 
@@ -99,7 +143,7 @@ def main(args):
         for gt_polygon in polygons_from_image:
             pred_texts.append(
                 get_pred_text_for_gt_polygon(
-                    gt_polygon, pred_data, args.evaluate_by_bbox)
+                    gt_polygon, pred_data, evaluate_by_bbox)
             )
 
         # to penalty false positive prediction, that were not matched with gt
@@ -125,7 +169,7 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--annotation_json_path', type=str, required=True,
                         help='Path to json with segmentation dataset'
-                        'annotation in COCO format')
+                        'annotation in COCO format.')
     parser.add_argument('--category_ids', nargs='+', type=int, required=True,
                         help='Category indexes (separated by spaces) from '
                         'annotation_json_path for evaluation.')
@@ -135,4 +179,5 @@ if __name__ == '__main__':
                         help='To evaluate prediction by bbox.')
     args = parser.parse_args()
 
-    main(args)
+    evaluate_pipeline(args.annotation_json_path, args.category_ids,
+                      args.pred_jsons_dir, args.evaluate_by_bbox)
