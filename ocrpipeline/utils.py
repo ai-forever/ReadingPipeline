778b4da2216e112cb5236972454c6b823695b4a1
Stanislav Kalinin <kalinin@sent.com>
2022-03-02 19:57:59 +0300

    chore: Add arg to get_image_visualization func to select classes for visualization

--- ocrpipeline/utils.py
+++ ocrpipeline/utils.py
@@ -7,17 +7,20 @@ def img_crop(image, bbox):
     return image[bbox[1]:bbox[3], bbox[0]:bbox[2]]
 
 
-def get_image_visualization(img, pred_data, fontpath, font_koef=50):
+def get_image_visualization(
+    img, pred_data, classes_to_draw, fontpath, font_koef=50
+):
     h, w = img.shape[:2]
     font = ImageFont.truetype(fontpath, int(h/font_koef))
     empty_img = Image.new('RGB', (w, h), (255, 255, 255))
     draw = ImageDraw.Draw(empty_img)
 
     for prediction in pred_data['predictions']:
-        contour = prediction['polygon']
-        pred_text = prediction['text']
-        cv2.drawContours(img, np.array([contour]), -1, (0, 255, 0), 2)
-        draw.text(min(contour), pred_text, fill=0, font=font)
+        if prediction['class_name'] in classes_to_draw:
+            contour = prediction['polygon']
+            pred_text = prediction['text']
+            cv2.drawContours(img, np.array([contour]), -1, (0, 255, 0), 2)
+            draw.text(min(contour), pred_text, fill=0, font=font)
 
     vis_img = np.array(empty_img)
     vis = np.concatenate((img, vis_img), axis=1)
