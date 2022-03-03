cf8ba926dec046ff8607775135786a0ce59b260c
Stanislav Kalinin <kalinin@sent.com>
2022-03-03 15:57:33 +0300

    chore: Add arg to get_image_visualization func to select rotated contours for visualization

--- ocrpipeline/utils.py
+++ ocrpipeline/utils.py
@@ -8,7 +8,9 @@ def img_crop(image, bbox):
 
 
 def get_image_visualization(
-    img, pred_data, classes_to_draw, fontpath, font_koef=50
+    img, pred_data, classes_to_draw, fontpath,
+    polygon_name='polygon', text_name='text',
+    font_koef=50
 ):
     h, w = img.shape[:2]
     font = ImageFont.truetype(fontpath, int(h/font_koef))
@@ -17,8 +19,8 @@ def get_image_visualization(
 
     for prediction in pred_data['predictions']:
         if prediction['class_name'] in classes_to_draw:
-            contour = prediction['polygon']
-            pred_text = prediction['text']
+            contour = prediction[polygon_name]
+            pred_text = prediction[text_name]
             cv2.drawContours(img, np.array([contour]), -1, (0, 255, 0), 2)
             draw.text(min(contour), pred_text, fill=0, font=font)
 
