d3e33dabf06d029922c61ce588ae716ca2a33d35
Stanislav Kalinin <kalinin@sent.com>
2022-03-16 12:21:34 +0300

    docs: Update docstring to ocrpipeline/predictor

--- ocrpipeline/predictor.py
+++ ocrpipeline/predictor.py
@@ -303,6 +303,10 @@ class PipelinePredictor:
                             'text': predicted text.
                             'rotated_bbox': [x_min, y_min, x_max, y_max] processed bbox for a rotated image with the restored angle
                             'rotated_polygon': [ [x1,y1], [x2,y2], ..., [xN,yN] ] processed polygon for a rotated image with the restored angle
+                            'polygon_center': [x, y] the center of the rotated_polygon.
+                            'page_idx': int, The page index of the polygon.
+                            'line_idx': int, The line index of the polygon within page index.
+                            'column_idx': int, The column index of the polygon within line index.
                         },
                         ...
                     ]
